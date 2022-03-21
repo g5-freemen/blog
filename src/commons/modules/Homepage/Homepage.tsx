0fd6827751007074b75a57981cec090845519381
g5-freemen <member@tut.by>
2022-03-21 19:35:17 +0300

    init Homepage

---
+++ src/commons/modules/Homepage/Homepage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Banner from '../../components/Banner/Banner';
+import Navbar from '../../components/Navbar/Navbar';
+
+export default function Homepage() {
+  return (
+    <div>
+      <Navbar />
+      <Banner />
+    </div>
+  );
+}
