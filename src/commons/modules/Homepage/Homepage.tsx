adf58ad7cb3f4895e9cca16ab93083327fa516b2
g5-freemen <member@tut.by>
2022-03-22 09:40:14 +0300

    fix: styles for Homepage, Navbar, Banner & CRLF

--- src/commons/modules/Homepage/Homepage.tsx
+++ src/commons/modules/Homepage/Homepage.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Banner from '../../components/Banner/Banner';
 import Navbar from '../../components/Navbar/Navbar';
+import styles from './Homepage.module.css';
 
 export default function Homepage() {
   return (
-    <div>
+    <div className={styles.container}>
       <Navbar />
       <Banner />
+      <div className={styles.row}>
+        <main className={styles.main}>0</main>
+        <aside className={styles.aside}>1</aside>
+      </div>
     </div>
   );
 }
