022552e3680873e44df3d044eaae29cd2d8aede8
g5-freemen <member@tut.by>
2022-04-06 17:19:26 +0300

    init SignUp

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Navbar from './commons/components/Navbar/Navbar';
 import Homepage from './commons/modules/Homepage/Homepage';
 import SignIn from './commons/modules/SignIn/SignIn';
 import SignUp from './commons/modules/SignUp/SignUp';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <ToastContainer />
       <Routes>
         <Route path="/login" element={<SignIn />} />
         <Route path="/register" element={<SignUp />} />
--- src/commons/modules/SignUp/SignUp.tsx
+++ src/commons/modules/SignUp/SignUp.tsx
@@ -1,24 +1,100 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useEffect } from 'react';
+import { toast } from 'react-toastify';
+import { useForm, FormProvider } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { useNavigate, Link } from 'react-router-dom';
 import { Button } from '../../components/Button/Button';
 import { Input } from '../../components/Input/Input';
 import styles from '../SignIn/SignIn.module.css';
+import { apiUrl } from '../../utils/constants';
+
+interface ISignUp {
+  username: string;
+  email: string;
+  password: string;
+}
+
+const schema = yup
+  .object()
+  .shape({
+    username: yup.string().required(),
+    email: yup.string().email().required(),
+    password: yup.string().required().min(6),
+  })
+  .required();
 
 export default function SignUp() {
+  const navigate = useNavigate();
+
+  const methods = useForm<ISignUp>({
+    mode: 'onChange',
+    resolver: yupResolver(schema),
+  });
+
+  const {
+    reset,
+    register,
+    handleSubmit,
+    formState: { isValid },
+  } = methods;
+
+  const onSubmit = async (data: ISignUp) => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ user: { ...data } }),
+    };
+    const response = await fetch(`${apiUrl}/api/users`, requestOptions);
+    const userdata = await response.json();
+
+    if (!response.ok) {
+      const errorsKeys = Object.keys(userdata.errors);
+      const errorsValues = Object.values(userdata.errors);
+      errorsKeys.forEach((key, i) => {
+        const keyCap = key[0].toUpperCase() + key.slice(1);
+        toast(`${keyCap} ${errorsValues[i]}`, { type: 'error' });
+      });
+      return false;
+    }
+
+    toast(`${userdata.user.username} Registered Successfully!`, {
+      type: 'success',
+    });
+    return navigate('/');
+  };
+
+  const submit = handleSubmit(onSubmit);
+
+  useEffect(() => {
+    reset();
+    return () => reset();
+  }, []);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Sign up</h1>
       <Link to={{ pathname: '/login' }} className={styles.link}>
         Have an account?
       </Link>
-      <form className={styles.form}>
-        <Input placeholder="Username" name="username" />
-        <Input placeholder="Email" />
-        <Input placeholder="Password" />
-        <div className={styles.right}>
-          <Button type="submit">Sign Up</Button>
-        </div>
-      </form>
+      <FormProvider {...methods}>
+        <form noValidate className={styles.form} onSubmit={submit}>
+          <Input placeholder="Username" {...register('username')} />
+          <Input type="email" placeholder="Email" {...register('email')} />
+          <Input
+            type="password"
+            autoComplete="current-password"
+            placeholder="Password"
+            {...register('password')}
+          />
+          <div className={styles.right}>
+            <Button type="submit" disabled={!isValid}>
+              Sign Up
+            </Button>
+          </div>
+        </form>
+      </FormProvider>
     </div>
   );
 }
