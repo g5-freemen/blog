63da4a5140d9d63f46f6ea2c69379643e893f85d
g5-freemen <member@tut.by>
2022-04-06 19:29:49 +0300

    refactor: fetchArticles type

--- src/commons/components/Articles/fetchArticles.ts
+++ src/commons/components/Articles/fetchArticles.ts
@@ -1,9 +1,9 @@
 import { apiUrl } from '../../utils/constants';
 import { ArticleType } from '../Article/Article';
 
-export async function fetchArticles(
-  limit: number,
-): Promise<ArticleType[] | null> {
+type ArticlesType = Promise<ArticleType[] | null>;
+
+export async function fetchArticles(limit: number): ArticlesType {
   try {
     const response = await fetch(`${apiUrl}/api/articles?limit=${limit}`);
     const data = await response.json();
