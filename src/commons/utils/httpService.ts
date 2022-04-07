ccc31a291059a96ea0e09742d88d2c1a4ccce4a8
g5-freemen <member@tut.by>
2022-04-07 15:47:32 +0300

    feat: httpService for user registration

---
+++ src/commons/utils/httpService.ts
@@ -0,0 +1,14 @@
+import { ISignUp } from '../modules/SignUp/SignUp';
+import { apiUrl } from './constants';
+
+export async function registerUser(formData: ISignUp) {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ user: { ...formData } }),
+  };
+  const response = await fetch(`${apiUrl}/api/users`, requestOptions);
+  const data = await response.json();
+
+  return { response, data };
+}
