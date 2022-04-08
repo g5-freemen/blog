6c3f5caceffc291efbb1b082036d8b46bce1b2b5
g5-freemen <member@tut.by>
2022-04-08 11:18:28 +0300

    fix: try/catch fetch

--- src/commons/utils/httpService.ts
+++ src/commons/utils/httpService.ts
@@ -2,13 +2,18 @@ import { ISignUp } from '../modules/SignUp/SignUp';
 import { apiUrl } from './constants';
 
 export async function registerUser(formData: ISignUp) {
-  const requestOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ user: { ...formData } }),
-  };
-  const response = await fetch(`${apiUrl}/api/users`, requestOptions);
-  const data = await response.json();
+  try {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ user: { ...formData } }),
+    };
+    const response = await fetch(`${apiUrl}/api/users`, requestOptions);
+    const data = await response.json();
 
-  return { response, data };
+    return { response, data };
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
 }
