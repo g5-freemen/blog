d5e32f136fe43f939c18e6bf602de6487a2efe5b
g5-freemen <member@tut.by>
2022-04-11 16:59:54 +0300

    fix: redux user

--- src/commons/redux/reducers/userReducer.ts
+++ src/commons/redux/reducers/userReducer.ts
@@ -12,7 +12,7 @@ export default function rootReducer(
 ) {
   switch (action.type) {
     case SET_USER:
-      return { ...state, limit: action.payload };
+      return { ...state, user: action.payload };
     default:
       return state;
   }
