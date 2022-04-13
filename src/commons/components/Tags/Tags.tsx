51b727ff31ccd8e270e997e1463b38e26029bc06
g5-freemen <member@tut.by>
2022-04-13 12:01:05 +0300

    init Navpills

--- src/commons/components/Tags/Tags.tsx
+++ src/commons/components/Tags/Tags.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { setActivePill } from '../../redux/reducers/feedReducer';
 import Tag from '../Tag/Tag';
 import styles from './Tags.module.css';
 
@@ -8,13 +10,18 @@ interface TagsProps {
 
 export default function Tags(props: TagsProps) {
   const { tagsList } = props;
+  const dispatch = useDispatch();
 
   return (
     <aside className={styles.column}>
       <ul className={styles.sidebar}>
         <p className={styles.p}>Popular Tags</p>
         {tagsList.map((el) => (
-          <Tag key={`tag-${el}`} name={el} />
+          <Tag
+            key={`tag-${el}`}
+            name={el}
+            onClick={() => dispatch(setActivePill(`#${el}`))}
+          />
         ))}
       </ul>
     </aside>
