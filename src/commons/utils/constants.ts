7ce7478c95e80048cac2b5f5e543d1dbfbcdc66c
g5-freemen <member@tut.by>
2022-05-04 16:50:18 +0300

    fix: some deps & consts

--- src/commons/utils/constants.ts
+++ src/commons/utils/constants.ts
@@ -1,5 +1,6 @@
 export const apiUrl = 'https://api.realworld.io';
 export const DEFAULT_ARTICLES_LIMIT = 20;
+export const DEFAULT_PERSONAL_ARTICLES_LIMIT = 5;
 export const MIN_PASSWORD_LENGTH = 6;
 
 export const headerContent = {
