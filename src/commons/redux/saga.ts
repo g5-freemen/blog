759d243309e617bf189d63f1f5b1c8f1d7068412
g5-freemen <member@tut.by>
2022-05-17 12:40:51 +0300

    init

--- src/commons/redux/saga.ts
+++ src/commons/redux/saga.ts
@@ -1,18 +1,16 @@
 import { takeEvery, put } from 'redux-saga/effects';
-// import { sagaActions } from './sagaActions';
-import { SET_TAGS } from './reducers/actions/feedActions';
+import { SET_ARTICLES, SET_TAGS } from './reducers/actions/feedActions';
+import {
+  SET_LOADING_ARTICLES,
+  SET_LOADING_TAGS,
+} from './reducers/actions/globalActions';
 
-export function* fetchDataSaga() {
-  try {
-    console.log(SET_TAGS);
-    // let result = yield call(() => {});
-    // yield put(fetchData(result.data));
-  } catch (e) {
-    yield put({ type: 'TODO_FETCH_FAILED' });
-  }
+export function* finishLoading(val: string) {
+  const loadedType = val === 'tags' ? SET_LOADING_TAGS : SET_LOADING_ARTICLES;
+  yield put({ type: loadedType, payload: false });
 }
 
 export default function* rootSaga() {
-  // yield takeEvery(sagaActions.FETCH_DATA_SAGA, fetchDataSaga);
-  yield takeEvery(SET_TAGS, fetchDataSaga);
+  yield takeEvery(SET_ARTICLES, () => finishLoading('articles'));
+  yield takeEvery(SET_TAGS, () => finishLoading('tags'));
 }
